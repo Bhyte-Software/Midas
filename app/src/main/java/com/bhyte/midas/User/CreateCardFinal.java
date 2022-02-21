6f9e72748a4cc167a7d160743d84bc38b91b8c9e
Braimah-Abiola <braimahabiola.official@gmail.com>
2022-02-21 23:18:45 +0000

    UI Updates

---
+++ app/src/main/java/com/bhyte/midas/User/CreateCardFinal.java
@@ -0,0 +1,14 @@
+package com.bhyte.midas.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateCardFinal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_card_final);
+    }
+}
---
+++ app/src/main/java/com/bhyte/midas/User/ProcessingCardCreation.java
@@ -0,0 +1,14 @@
+package com.bhyte.midas.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProcessingCardCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_processing_card_creation);
+    }
+}
