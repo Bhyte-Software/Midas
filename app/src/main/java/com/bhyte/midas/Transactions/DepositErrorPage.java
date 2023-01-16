8735a65df110d3e9c23546a30deb54b0f94d10aa
Nnamdi600000 <63124115+Nnamdi600000@users.noreply.github.com>
2023-01-16 10:23:28 -0800

    created and implemented success and error page also created the confirm deposit bottom sheet layout

---
+++ app/src/main/java/com/bhyte/midas/Transactions/DepositErrorPage.java
@@ -0,0 +1,14 @@
+package com.bhyte.midas.Transactions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DepositErrorPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deposit_error_page);
+    }
+}
