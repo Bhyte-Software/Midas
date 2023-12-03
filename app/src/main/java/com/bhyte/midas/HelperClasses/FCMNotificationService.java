958681a1c07974ebe93d45b6e0de0715af2de41c
JojoDuke <jojoamankwa@gmail.com>
2023-12-03 19:06:55 +0000

    Messages for when a user deposits and sends

--- app/src/main/java/com/bhyte/midas/HelperClasses/FCMNotificationService.java
+++
@@ -1,7 +0,0 @@
-package com.bhyte.midas.HelperClasses;
-
-import com.google.firebase.messaging.FirebaseMessagingService;
-
-public class FCMNotificationService extends FirebaseMessagingService {
-
-}
